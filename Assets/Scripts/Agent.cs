bf3c1ca109cc195c0834cb1ede162f75879457c7
Charles Snead <csnead86@gmail.com>
2025-02-19 11:55:25 -0800

    Maze generation with collision

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Agent : MonoBehaviour
+{
+    public int keys = 0;
+    public float speed = 15.0f;
+    // public GameObject door;
+
+    public Text keyAmount;
+    public Text youWin;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+             transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, speed * Time.deltaTime, 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -speed * Time.deltaTime, 0);
+        }
+
+        // if(keys == 3)
+        // {
+        //     Destroy(door);
+        // }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Keys")
+        {
+            keys++;
+            keyAmount.text = "Keys: " + keys;
+            Destroy(collision.gameObject);
+        }
+
+        if(collision.gameObject.tag == "Door") // silver
+        {
+            // Destroy(collision.gameObject);
+            // youWin.text = "YOU WIN!!!";
+            Debug.Log("You Win!!!");
+        }
+
+        if(collision.gameObject.tag == "Enemies")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if(collision.gameObject.tag == "Walls")
+        {
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+                Debug.Log("Left");
+            }
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+                Debug.Log("Right");
+            }
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+                Debug.Log("Up");
+            }
+            if(Input.GetKey(KeyCode.DownArrow))
+            {
+                transform.Translate(0, speed * Time.deltaTime, 0);
+                Debug.Log("Down");
+            }
+        }
+    }
+}
